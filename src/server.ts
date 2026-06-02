fc7844c2da54ca60fdbe3614acff1ff8607e1676
olinicker <linicker.ostroski@alunos.ifsuldeminas.edu.br>
2026-06-02 09:22:40 -0300

    feat: terceira aula

--- src/server.ts
+++ src/server.ts
@@ -1,19 +1,17 @@
-import fastify from "fastify";
+import fastify from 'fastify'
+import { setupKnex } from './database.js'
 
-const app = fastify();
+const app = fastify()
 
-// GET, POST, PUT, PATCH, DELETE
-
-// http://localhost:3333/hello
-
-app.get("/hello", () => {
-  return "Hello World";
-});
+app.get('/hello', async () => {
+  const tables = await setupKnex('sqlite_schema').select('*')
+  return tables
+})
 
 app
   .listen({
     port: 3333,
   })
   .then(() => {
-    console.log("HTTP Server Running");
-  });
+    console.log('HTTP Server Running')
+  })
